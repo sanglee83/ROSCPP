ce4ea9fa09ef546a5f15bb660203aa664f0f2e10
haseongju <haseongju@rideflux.com>
2021-10-14 15:11:02 +0900

    ROS Study 1, 2
      [Desc]
    
      [+]  ROS Study 1, 2

---
+++ TEST/src/node.cpp
@@ -0,0 +1,12 @@
+#include "ros/ros.h"  //ROS 기본 헤더파일
+ 
+int main(int argc, char **argv) //c++ 의 기본 함수형태
+{
+    ros::init(argc, argv, "test_node"); //노드명 초기화
+    ros::NodeHandle nh; //ROS시스템과 통신을 위한 노드핸들 선언
+ 
+ 
+    std::cout << "~~~~ WELCOME TO ROS WORLD ~~~~~~" << std::endl; // Print 
+
+    return 0;
+}
